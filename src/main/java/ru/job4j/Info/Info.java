981017df9d63acf7a5503a14d01a93a8ad2b17f5
rockfornik <nekmokshan@mail.ru>
2023-04-11 15:56:57 +0300

    Фиксация изменений на github [4]

--- src/main/java/ru/job4j/Info/Info.java
+++ src/main/java/ru/job4j/Info/Info.java
@@ -2,6 +2,6 @@ package ru.job4j.Info;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("11.04.2024");
+        System.out.println("12.04.2024");
     }
 }
